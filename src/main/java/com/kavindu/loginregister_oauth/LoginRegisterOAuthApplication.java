f6ffefce53835e86ea35e3bf5bc9c5af7c7d787a
KDDilshan <kdprojects847@gmail.com>
2024-11-25 19:49:26 +0530

    added new oauth opropertise

---
+++ src/main/java/com/kavindu/loginregister_oauth/LoginRegisterOAuthApplication.java
@@ -0,0 +1,13 @@
+package com.kavindu.loginregister_oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginRegisterOAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginRegisterOAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kavindu/loginregister_oauth/LoginRegisterOAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kavindu.loginregister_oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginRegisterOAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
